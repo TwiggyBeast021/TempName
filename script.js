092ae045325190ccfa8949f17967d52fab39e33f
TwiggyBeast021 <139478900+TwiggyBeast021@users.noreply.github.com>
2023-07-13 15:20:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,47 @@ let level = 1;
 // Get DOM elements
 const scoreElement = document.getElementById("score");
 const levelElement = document.getElementById("level");
-const encounterButton = document.getElementById("encounter");
+const increaseScoreButton = document.getElementById("increaseScoreButton");
+const increaseLevelButton = document.getElementById("increaseLevelButton");
+const logList = document.getElementById("logList");
+const progressBar = document.querySelector("#progressBar .progress");
 
 // Handle encounter button click
-encounterButton.addEventListener("click", function() {
-    // Generate a random encounter value
-    const encounterValue = Math.floor(Math.random() * 10) + 1;
-    
-    // Update score and level
-    score += encounterValue;
-    level = Math.floor(score / 10) + 1;
-    
-    // Update DOM elements
-    scoreElement.textContent = "Score: " + score;
-    levelElement.textContent = "Level: " + level;
+increaseScoreButton.addEventListener("click", function() {
+    increaseStat("score", 1);
 });
+
+increaseLevelButton.addEventListener("click", function() {
+    increaseStat("level", 1);
+});
+
+// Function to increase stats
+function increaseStat(stat, amount) {
+    const interval = setInterval(function() {
+        if (score >= amount) {
+            clearInterval(interval);
+            score -= amount;
+            level = Math.floor(score / 10) + 1;
+            updateStats();
+        }
+    }, 1000);
+
+    progressBar.style.width = "0%";
+    progressBar.style.transition = "width " + amount + "s";
+
+    setTimeout(function() {
+        progressBar.style.width = "100%";
+    }, 100);
+
+    setTimeout(function() {
+        clearInterval(interval);
+        updateStats();
+    }, amount * 1000);
+}
+
+// Update stats and log
+function updateStats() {
+    scoreElement.textContent = "Score: " + Math.round(score);
+    levelElement.textContent = "Level: " + Math.round(level);
+    logList.innerHTML += "<li>Score increased to " + Math.round(score) + ", Level increased to " + Math.round(level) + "</li>";
+}
