2bff5c5893c0a1356615df6cd0e22c2fa6252967
TwiggyBeast021 <139478900+TwiggyBeast021@users.noreply.github.com>
2023-07-13 12:15:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,110 @@
-// Variables for tracking game progress
+javascript
 let energy = 0;
-let generator1Cost = 10;
-let modifier1Cost = 100;
-let generatorProgress = 0;
-let modifierProgress = 0;
+let metal = 0;
+let energyGenerators = 0;
+let metalGenerators = 0;
+let adventureProgress = 0;
 
-// Function to update the energy display with rounded values
-function updateEnergyDisplay() {
-  document.getElementById("energyDisplay").textContent = Math.round(energy);
-}
+const energyElement = document.getElementById('energy');
+const metalElement = document.getElementById('metal');
+const scavengeButton = document.getElementById('scavenge');
+const generateEnergyButton = document.getElementById('generateEnergy');
+const createFlashlightButton = document.getElementById('createFlashlight');
+const createGeneratorButton = document.getElementById('createGenerator');
+const adventureButton = document.getElementById('adventure');
+const adventureProgressbar = document.getElementById('adventureProgressbar');
+const adventureLog = document.getElementById('adventureLog');
 
-// Function to handle the click button click event
-function handleClick() {
-  energy++;
-  updateEnergyDisplay();
-}
+scavengeButton.addEventListener('click', scavenge);
+generateEnergyButton.addEventListener('click', generateEnergy);
+createFlashlightButton.addEventListener('click', createFlashlight);
+createGeneratorButton.addEventListener('click', createGenerator);
+adventureButton.addEventListener('click', goAdventure);
 
-// Function to handle generator purchase
-function purchaseGenerator() {
-  if (energy >= generator1Cost) {
-    energy -= generator1Cost;
-    generator1Cost *= 1.5; // Increase the cost for the next generator
+function scavenge() {
+  scavengeButton.disabled = true;
+  setTimeout(() => {
+    const randomMetal = Math.floor(Math.random() * 3) + 1;
+    metal += randomMetal;
+    metalElement.textContent = metal;
+    scavengeButton.disabled = false;
 
-    // Enable the next generator after a certain progress
-    if (generatorProgress >= 100) {
-      // Enable the generator button
-      document.getElementById("generator2").disabled = false;
+    if (metal >= 10) {
+      createFlashlightButton.disabled = false;
     }
+  }, 5000);
+}
 
-    updateEnergyDisplay();
-    updateGeneratorProgress();
+function generateEnergy() {
+  generateEnergyButton.disabled = true;
+  setTimeout(() => {
+    energy += 3;
+    energyElement.textContent = energy;
+    generateEnergyButton.disabled = false;
+
+    if (energy >= 10 && metal >= 5) {
+      createGeneratorButton.disabled = false;
+    }
+  }, 1000);
+}
+
+function createFlashlight() {
+  if (energy >= 10 && metal >= 5) {
+    energy -= 10;
+    metal -= 5;
+    energyElement.textContent = energy;
+    metalElement.textContent = metal;
+    createFlashlightButton.disabled = true;
+    scavengeButton.addEventListener('click', scavenge);
+
+    if (energy >= 25 && metal >= 50) {
+      createGeneratorButton.disabled = false;
+    }
   }
 }
 
-// Function to handle modifier purchase
-function purchaseModifier() {
-  if (energy >= modifier1Cost) {
-    energy -= modifier1Cost;
-    modifier1Cost *= 2; // Increase the cost for the next modifier
-
-    // Enable the next modifier after a certain progress
-    if (modifierProgress >= 100) {
-      // Enable the modifier button
-      document.getElementById("modifier2").disabled = false;
-    }
-
-    updateEnergyDisplay();
-    updateModifierProgress();
+function createGenerator() {
+  if (energy >= 25 && metal >= 50) {
+    energy -= 25;
+    metal -= 50;
+    energyGenerators++;
+    energyElement.textContent = energy;
+    metalElement.textContent = metal;
+    createGeneratorButton.disabled = true;
+    adventureButton.disabled = false;
   }
 }
 
-// Function to update the generator purchase progress
-function updateGeneratorProgress() {
-  generatorProgress = Math.min((energy / generator1Cost) * 100, 100);
-  document.getElementById("generatorProgress").value = generatorProgress;
+function goAdventure() {
+  adventureButton.disabled = true;
+  adventureProgress = 0;
+  adventureLog.innerHTML = '';
+
+  const adventureInterval = setInterval(() => {
+    adventureProgress += 10;
+    adventureProgressbar.style.width = `${adventureProgress}%`;
+
+    if (adventureProgress >= 100) {
+      clearInterval(adventureInterval);
+      adventureButton.disabled = false;
+      adventureProgressbar.style.width = '0%';
+
+      const randomEvent = Math.floor(Math.random() * 3);
+      switch (randomEvent) {
+        case 0:
+          adventureLog.innerHTML += '<p>You found a lost treasure chest filled with Energy!</p>';
+          energy += 50;
+          energyElement.textContent = energy;
+          break;
+        case 1:
+          adventureLog.innerHTML += '<p>You encountered a group of friendly survivors who offered you Metal!</p>';
+          metal += 25;
+          metalElement.textContent = metal;
+          break;
+        case 2:
+          adventureLog.innerHTML += '<p>You stumbled upon a mysterious ruin, but found nothing of value.</p>';
+          break;
+      }
+    }
+  }, 100);
 }
-
-// Function to update the modifier purchase progress
-function updateModifierProgress() {
-  modifierProgress = Math.min((energy / modifier1Cost) * 100, 100);
-  document.getElementById("modifierProgress").value = modifierProgress;
-}
-
-// Function to handle prestige
-function prestige() {
-  // Implement your own logic for prestige
-
-  // Reset game progress
-
-  // Add bonus or multiplier to energy production
-
-  // Disable all generators and modifiers
-
-  // Enable prestige again after reaching certain conditions
-}
-
-// Add event listeners to buttons
-document.getElementById("clickButton").addEventListener("click", handleClick);
-document.getElementById("generator1").addEventListener("click", purchaseGenerator);
-document.getElementById("modifier1").addEventListener("click", purchaseModifier);
-document.getElementById("prestigeButton").addEventListener("click", prestige);
-
-// Add smooth energy increase every second
-setInterval(function() {
-  energy += generatorProgress / 10; // Adjust the increment based on progress
-  updateEnergyDisplay();
-}, 1000);
